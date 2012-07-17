16aa206f24b8e7c85a9b071c26fd1ee9cdaba3d5
Sota <sota@pankaku.com>
2012-07-17 23:07:01 +0900

    Refactoring at mouse event handlers of piano roll.

--- SaltCase/UI/SCPianoRoll.m
+++ SaltCase/UI/SCPianoRoll.m
@@ -33,7 +33,6 @@
     }
 }
 
-#pragma mark Mouse events
 - (double)beatPositionAtPoint:(NSPoint)point {
     return point.x / kSCPianoRollHorizontalGridInterval;
 }
@@ -44,20 +43,22 @@
 - (NSPoint)pointOfEvent:(NSEvent*)event {
     return [self convertPoint:event.locationInWindow fromView:nil];
 }
-- (void)mouseDown:(NSEvent *)theEvent {
-    NSPoint cursorAt = [self pointOfEvent:theEvent];
-    NSLog(@"mouseDown at (%f, %d)", [self beatPositionAtPoint:cursorAt], [self pitchNumberAtPoint:cursorAt]);
-    
-    float y = [self pitchNumberAtPoint:cursorAt] * kSCNoteLineHeight;
-    NSButton* button = [[NSButton alloc] initWithFrame:NSMakeRect(cursorAt.x, y, 50.0f, kSCNoteLineHeight)];
-    [self addSubview:button];
-    self.selectedNote = button;
-}
 - (void)moveSelectedNoteTo:(NSPoint)cursorAt {
     if (self.selectedNote) {
         float y = [self pitchNumberAtPoint:cursorAt] * kSCNoteLineHeight;
         self.selectedNote.frame = NSMakeRect(cursorAt.x, y, self.selectedNote.frame.size.width, kSCNoteLineHeight);
     }
+}
+
+#pragma mark Mouse events
+
+- (void)mouseDown:(NSEvent *)theEvent {
+    NSPoint cursorAt = [self pointOfEvent:theEvent];
+    
+    float y = [self pitchNumberAtPoint:cursorAt] * kSCNoteLineHeight;
+    NSButton* button = [[NSButton alloc] initWithFrame:NSMakeRect(cursorAt.x, y, 50.0f, kSCNoteLineHeight)];
+    [self addSubview:button];
+    self.selectedNote = button;
 }
 - (void)mouseDragged:(NSEvent *)theEvent {
     [self moveSelectedNoteTo:[self pointOfEvent:theEvent]];
