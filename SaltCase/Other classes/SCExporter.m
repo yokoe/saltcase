1415c27b794796777285f85fa174fb8101227045
Sota <sota@pankaku.com>
2012-08-13 21:11:14 +0900

    Exporter creates file.

--- SaltCase/Other classes/SCExporter.m
+++ SaltCase/Other classes/SCExporter.m
@@ -8,6 +8,9 @@
 
 #import "SCExporter.h"
 
+#import <AudioToolbox/AudioToolbox.h>
+#import <AudioToolbox/ExtendedAudioFile.h>
+
 @interface SCExporter() {
     NSURL* url_;
     SCExportStyle style_;
@@ -24,6 +27,36 @@
     return self;
 }
 - (void)export {
-    NSLog(@"export");
+    OSStatus error = noErr;
+    AudioStreamBasicDescription outFormat = [self outputFormat];
+    ExtAudioFileRef outFileRef;
+    
+    // Create a file
+    error = ExtAudioFileCreateWithURL((__bridge CFURLRef)url_, kAudioFileWAVEType, &outFormat, NULL, 0, &outFileRef);
+    if (error != noErr) {
+        NSLog(@"Failed to create file %@", url_);
+        goto ExitExport;
+    }
+    
+    // Close file
+    ExtAudioFileDispose(outFileRef);
+    
+    return;
+ExitExport:
+    if (outFileRef) ExtAudioFileDispose(outFileRef);
+    NSLog(@"Export failed.");
+}
+
+- (AudioStreamBasicDescription)outputFormat {
+    AudioStreamBasicDescription format;
+    format.mSampleRate = 44100;
+    format.mFormatID = kAudioFormatLinearPCM;
+    format.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
+    format.mBitsPerChannel = 16;
+    format.mChannelsPerFrame = 2;
+    format.mFramesPerPacket = 1;
+    format.mBytesPerFrame =	format.mBitsPerChannel / 8 * format.mChannelsPerFrame;
+    format.mBytesPerPacket = format.mBytesPerFrame * format.mFramesPerPacket;
+    return format;
 }
 @end
